dc461231a0d68d7484bdca7f4eba65acc51a36db
unesexact <oukessouyounes1@gmail.com>
2026-07-07 16:07:25 +0100

    Home Screen UI

--- app/src/main/java/com/example/expensetracker/activities/MainActivity.java
+++ app/src/main/java/com/example/expensetracker/activities/MainActivity.java
@@ -7,16 +7,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.expensetracker.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.rvExpenses);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
